9bf945307d3666ecf321c21dad2a1b51c0cf148d
Colton <colton@coltonbrown.co>
2020-04-04 23:13:58 -0500

    add s3 compression

---
+++ backend/updateState/convert-image.js
@@ -0,0 +1,18 @@
+var AWS = require('aws-sdk')
+var constants = require('../constants');
+
+var s3 = new AWS.S3();
+
+module.exports.convertB64Image = function convertB64Image({ rawImage, gameId }) {
+  const base64Data = new Buffer(rawImage.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+  const type = rawImage.split(';')[0].split('/')[1]
+  const params = {
+    Bucket: constants.BUCKETS.IMAGES,
+    Key: `games/${ gameId }`,
+    Body: base64Data,
+    ACL: 'public-read',
+    ContentEncoding: 'base64',
+    ContentType: `image/${ type }`
+  };
+  return s3.upload(params).promise().then( ({ Location }) => Location );
+}
