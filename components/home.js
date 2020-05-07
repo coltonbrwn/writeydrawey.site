7feada52d31bd4cc9b840a1bd20af81eed60eaa2
Colton <colton@coltonbrown.co>
2020-05-07 18:02:15 -0500

    add rules page, improve 404 handling

--- components/home.js
+++ components/home.js
@@ -1,46 +1,23 @@
-
-import Link from 'next/link'
-
 import Logo from './svg/logo';
-import * as api from '../lib/api';
+import Nav from './nav';
 
 export default class Home extends React.Component {
-
-  onCreateClick = async () => {
-    let id;
-    try {
-      const {response:{id}} = await api.createNewGame();
-      document.location = `/${ id }`;
-    } catch (err) {
-      console.log(err)
-    }
-  }
 
   render() {
     return (
       <div className="full-height">
-        <div className="nav">
-          <div>
-            <Link href="/">
-              <a>New Game</a>
-            </Link>
-          </div>
-          <div>
-            <Link href="/rules">
-              <a>Rules</a>
-            </Link>
-          </div>
-        </div>
+        <Nav noLogo noHome={ this.props.statusCode !== 404 }/>
         <div className="home">
-          <div className="logo">
-            <Logo />
-          </div>
+          {
+            this.props.statusCode == 404 ? (
+              <p>We couldn't find a game here</p>
+            ) : (
+              <div className="logo">
+                <Logo />
+              </div>
+            )
+          }
         </div>
-        {
-          this.props.statusCode == 404 && (
-            <p>We couldn't find a game here</p>
-          )
-        }
       </div>
     )
   }
