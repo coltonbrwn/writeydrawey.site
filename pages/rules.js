8d5899b3a82f8003c6a1d604f39d17213cfe5a0a
Colton <colton@coltonbrown.co>
2020-05-09 19:28:35 -0500

    playing pages and styles

--- pages/rules.js
+++ pages/rules.js
@@ -8,14 +8,16 @@ export default function(props) {
     <Layout theme="light">
       <div className="full-height">
         <Nav noLogo/>
-        <div className="rules">
-          <h1>Rules</h1>
-          <p>
-            Writey Drawey is kind of like <i>telephone</i> meets <i>pictionary</i>. First, each player chooses a secret phrase. In the first round, the phrases are rotated around the circle, and each player attempts to illustrate the phrase they are given. Next, the drawings rotate, and players describe with words the drawing they’ve received. The cycle continues until the game is over.
-          </p>
-          <p>
-            To start a new game, click <u>new game</u>, then copy and share the unique game link with your friends
-          </p>
+        <div className="content-container">
+          <div className="rules">
+            <h1 className="mono">Rules</h1>
+            <p>
+              Writey Drawey is kind of like <i>telephone</i> meets <i>pictionary</i>. First, each player chooses a secret phrase. In the first round, the phrases are rotated around the circle, and each player attempts to illustrate the phrase they are given. Next, the drawings rotate, and players describe with words the drawing they’ve received. The cycle continues until the game is over.
+            </p>
+            <p>
+              To start a new game, click <u>new game</u>, then copy and share the unique game link with your friends
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
