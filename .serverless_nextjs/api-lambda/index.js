104a83410b3ec1295cdbf9df519066cb5eb6cfdc
Colton <colton@coltonbrown.co>
2020-05-14 13:24:20 -0500

    deploy

--- .serverless_nextjs/api-lambda/index.js
+++ .serverless_nextjs/api-lambda/index.js
@@ -14,15 +14,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const manifest_json_1 = __importDefault(require("./manifest.json"));
 const next_aws_cloudfront_1 = __importDefault(require("next-aws-cloudfront"));
+const normaliseUri = (uri) => (uri === "/" ? "/index" : uri);
 const router = (manifest) => {
-    const { pages: { ssr, html } } = manifest;
-    const allDynamicRoutes = Object.assign(Object.assign({}, ssr.dynamic), html.dynamic);
+    const { apis: { dynamic, nonDynamic } } = manifest;
     return (path) => {
-        if (ssr.nonDynamic[path]) {
-            return ssr.nonDynamic[path];
+        if (nonDynamic[path]) {
+            return nonDynamic[path];
         }
-        for (const route in allDynamicRoutes) {
-            const { file, regex } = allDynamicRoutes[route];
+        for (const route in dynamic) {
+            const { file, regex } = dynamic[route];
             const re = new RegExp(regex, "i");
             const pathMatchesRoute = re.test(path);
             if (pathMatchesRoute) {
@@ -32,31 +32,12 @@ const router = (manifest) => {
         return "pages/_error.js";
     };
 };
-const normaliseUri = (uri) => (uri === "/" ? "/index" : uri);
 exports.handler = (event) => __awaiter(void 0, void 0, void 0, function* () {
     const request = event.Records[0].cf.request;
     const uri = normaliseUri(request.uri);
-    const manifest = manifest_json_1.default;
-    const { pages, publicFiles } = manifest;
-    const isStaticPage = pages.html.nonDynamic[uri];
-    const isPublicFile = publicFiles[uri];
-    const origin = request.origin;
-    const s3Origin = origin.s3;
-    if (isStaticPage || isPublicFile) {
-        s3Origin.path = isStaticPage ? "/static-pages" : "/public";
-        if (isStaticPage) {
-            request.uri = uri + ".html";
-        }
-        return request;
-    }
-    const pagePath = router(manifest)(uri);
-    if (pagePath.endsWith(".html")) {
-        s3Origin.path = "/static-pages";
-        request.uri = pagePath.replace("pages", "");
-        return request;
-    }
-    const { req, res, responsePromise } = next_aws_cloudfront_1.default(event.Records[0].cf);
+    const pagePath = router(manifest_json_1.default)(uri);
     const page = require(`./${pagePath}`);
-    page.render(req, res);
+    const { req, res, responsePromise } = next_aws_cloudfront_1.default(event.Records[0].cf);
+    page.default(req, res);
     return responsePromise;
 });
